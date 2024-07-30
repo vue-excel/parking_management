92d485eba23bddc8b4970d1fd6483ebe41e6f907
vue-excel <xspace781@gmail.com>
2024-07-30 18:50:06 +0530

    worked on register form

---
+++ src/components/RegisterForm.vue
@@ -0,0 +1,49 @@
+<template>
+    <div>
+        <v-form @submit.prevent="register">
+
+            <v-text-field
+                    v-model="name"
+                    label="Имя"
+                    required
+                    :rules="nameRules"
+                    :error-messages="errors.name"
+            ></v-text-field>
+
+            <v-text-field
+                    v-model="email"
+                    label="E-mail"
+                    required
+                    :rules="emailRules"
+                    :error-messages="errors.email"
+            ></v-text-field>
+
+            <v-text-field
+                    v-model="password"
+                    label="Пароль"
+                    type="password"
+                    required
+                    :rules="passwordRules"
+                    :error-messages="errors.password"
+            ></v-text-field>
+
+            <v-text-field
+                    v-model="password2"
+                    label="Пароль еще раз"
+                    type="password"
+                    required
+                    :rules="passwordRules"
+                    :error-messages="errors.password2"
+            ></v-text-field>
+
+            <v-btn type="submit" color="primary" :disabled="loading" :loading="loading">{{$t('register')}}</v-btn>
+
+        </v-form>
+    </div>
+</template>
+
+
+
+<style scoped>
+
+</style>
